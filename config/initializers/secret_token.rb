0e73f52c13100783a0990dc2887a462f47ea227f
edannunziata <slice@twitchgames.com>
2011-05-29 12:39:49 -0700

    first commit, yet again

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xlgcblog::Application.config.secret_token = 'f45d3562c264e95d48bb2f13f06afe89e17383ea27c74a3c37679985fbce7a80a64c8d4cbf514c82503a3e4c94650e6747850dea2618f0278c23bf26861849b1'
